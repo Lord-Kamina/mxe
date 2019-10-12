fb6e734446177637a360486e3be86e879830d4ce
Gregorio Litenstein <g.litenstein@gmail.com>
2019-10-11 22:26:52 -0300

    ilmbase: Update to 2.4.0, build with cmake.
    Also, add patches to support building with pthreads (I’ve submitted a PR with them for these also)

--- src/ilmbase.mk
+++ src/ilmbase.mk
@@ -4,12 +4,12 @@ PKG             := ilmbase
 $(PKG)_WEBSITE  := https://www.openexr.com/
 $(PKG)_DESCR    := IlmBase
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.3.0
-$(PKG)_CHECKSUM := 456978d1a978a5f823c7c675f3f36b0ae14dba36638aeaa3c4b0e784f12a3862
-$(PKG)_SUBDIR   := ilmbase-$($(PKG)_VERSION)
-$(PKG)_FILE     := ilmbase-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://download.savannah.nongnu.org/releases/openexr/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc
+$(PKG)_VERSION  := 2.4.0
+$(PKG)_CHECKSUM := 4904c5ea7914a58f60a5e2fbc397be67e7a25c380d7d07c1c31a3eefff1c92f1
+$(PKG)_GH_CONF  := openexr/openexr/releases,v
+$(PKG)_FILE     := openexr-v$($(PKG)_VERSION).tar.gz
+$(PKG)_SUBDIR   := openexr-$($(PKG)_VERSION)
+$(PKG)_DEPS     := cc $(BUILD)~cmake
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://www.openexr.com/downloads.html' | \
@@ -24,18 +24,18 @@ GCC_VERSION_MINOR := $(shell echo $(gcc_VERSION) | cut -f2 -d.)
 $(PKG)_CXXSTD_14 := $(shell [ $(GCC_VERSION_MAJOR) -gt 6 -o \( $(GCC_VERSION_MAJOR) -eq 6 -a $(GCC_VERSION_MINOR) -ge 1 \) ] && echo true)
 
 define $(PKG)_BUILD
-    cd '$(1)' && $(SHELL) ./configure \
-        $(MXE_CONFIGURE_OPTS) \
-        --enable-threading \
-        --disable-posix-sem \
-        --enable-cxxstd=$(if $($(PKG)_CXXSTD_14),14,11) \
-        CONFIG_SHELL=$(SHELL) \
-        SHELL=$(SHELL)
-    # do the first build step by hand, because programs are built that
-    # generate source files
-    cd '$(1)/Half' && $(BUILD_CXX) eLut.cpp -o eLut
-    '$(1)/Half/eLut' > '$(1)/eLut.h'
-    cd '$(1)/Half' && $(BUILD_CXX) toFloat.cpp -o toFloat
-    '$(1)/Half/toFloat' > '$(1)/toFloat.h'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install bin_PROGRAMS= sbin_PROGRAMS= noinst_PROGRAMS=
+    mkdir '$(BUILD_DIR)/native'
+    mkdir '$(BUILD_DIR)/cross'
+    cd '$(BUILD_DIR)/native' && cmake \
+    -DOPENEXR_CXX_STANDARD=$(if $($(PKG)_CXXSTD_14),14,11) \
+    "$(SOURCE_DIR)"/IlmBase
+    $(MAKE) -C '$(BUILD_DIR)/native/Half' -j '$(JOBS)'
+
+    cd '$(BUILD_DIR)/cross' && $(TARGET)-cmake \
+    -DOPENEXR_CXX_STANDARD=$(if $($(PKG)_CXXSTD_14),14,11) \
+    -DNATIVE_ILMBASE_BUILD_DIR='$(BUILD_DIR)/native' \
+    -DBUILD_TESTING=OFF \
+    "$(SOURCE_DIR)"/IlmBase
+    $(MAKE) -C '$(BUILD_DIR)/cross' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)/cross' -j 1 install
 endef
