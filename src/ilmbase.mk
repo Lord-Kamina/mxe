cd9de21735fe60fa329503e3267620454676f70e
Gregorio Litenstein <g.litenstein@gmail.com>
2020-06-12 11:45:39 -0400

    IlmBase / OpenEXR: Update to 2.5.1, remove patches
    My fixes have been merged upstream, rendering patches innecessary and greatly simplifying builds.

--- src/ilmbase.mk
+++ src/ilmbase.mk
@@ -4,8 +4,8 @@ PKG             := ilmbase
 $(PKG)_WEBSITE  := https://www.openexr.com/
 $(PKG)_DESCR    := IlmBase
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.4.0
-$(PKG)_CHECKSUM := 4904c5ea7914a58f60a5e2fbc397be67e7a25c380d7d07c1c31a3eefff1c92f1
+$(PKG)_VERSION  := 2.5.1
+$(PKG)_CHECKSUM := 11f806bf256453e39fc33bd1cf1fa576a54f144cedcdd3e6935a177e5a89d02e
 $(PKG)_GH_CONF  := AcademySoftwareFoundation/openexr/releases,v
 $(PKG)_FILE     := openexr-v$($(PKG)_VERSION).tar.gz
 $(PKG)_SUBDIR   := openexr-$($(PKG)_VERSION)
@@ -21,21 +21,14 @@ endef
 GCC_VERSION_MAJOR := $(shell echo $(gcc_VERSION) | cut -f1 -d.)
 GCC_VERSION_MINOR := $(shell echo $(gcc_VERSION) | cut -f2 -d.)
 
-$(PKG)_CXXSTD_14 = $(shell if [[ $(GCC_VERSION_MAJOR) > 6 || \( $(GCC_VERSION_MAJOR) == 6 && $(GCC_VERSION_MINOR) >= 1 \) ]]; then echo true)
+$(PKG)_CXXSTD_14 = `[ $(GCC_VERSION_MAJOR) > 6 || \( $(GCC_VERSION_MAJOR) == 6 && $(GCC_VERSION_MINOR) >= 1 \) ] && echo true`
 
 define $(PKG)_BUILD
-    mkdir '$(BUILD_DIR)/native'
-    mkdir '$(BUILD_DIR)/cross'
-    cd '$(BUILD_DIR)/native' && cmake \
+    cd '$(BUILD_DIR)' && $(TARGET)-cmake \
     -DOPENEXR_CXX_STANDARD=$(if $($(PKG)_CXXSTD_14),14,11) \
-    "$(SOURCE_DIR)"/IlmBase
-    $(MAKE) -C '$(BUILD_DIR)/native/Half' -j '$(JOBS)'
-
-    cd '$(BUILD_DIR)/cross' && $(TARGET)-cmake \
-    -DOPENEXR_CXX_STANDARD=$(if $($(PKG)_CXXSTD_14),14,11) \
-    -DNATIVE_ILMBASE_BUILD_DIR='$(BUILD_DIR)/native' \
     -DBUILD_TESTING=OFF \
+    -DILMBASE_INSTALL_PKG_CONFIG=ON \
     "$(SOURCE_DIR)"/IlmBase
-    $(MAKE) -C '$(BUILD_DIR)/cross' -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)/cross' -j 1 install
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 endef
--- src/openexr.mk
+++ src/openexr.mk
@@ -1,16 +1,16 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
-PKG             := openexr
-$(PKG)_WEBSITE  := https://www.openexr.com/
-$(PKG)_DESCR    := OpenEXR
+PKG              := openexr
+$(PKG)_WEBSITE   := https://www.openexr.com/
+$(PKG)_DESCR     := OpenEXR
 $(PKG)_IGNORE    = $(ilmbase_IGNORE)
-$(PKG)_VERSION  := $(ilmbase_VERSION)
-$(PKG)_CHECKSUM := $(ilmbase_CHECKSUM)
-$(PKG)_SUBDIR   := openexr-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(ilmbase_FILE)
-$(PKG)_URL      := $(ilmbase_URL)
-$(PKG)_DEPS     := cc ilmbase pthreads zlib $(BUILD)~cmake
-$(PKG)_PATCHES  := $(ilmbase_PATCHES)
+$(PKG)_VERSION   = $(ilmbase_VERSION)
+$(PKG)_CHECKSUM  = $(ilmbase_CHECKSUM)
+$(PKG)_SUBDIR    = openexr-$($(PKG)_VERSION)
+$(PKG)_FILE      = $(ilmbase_FILE)
+$(PKG)_URL       = $(ilmbase_URL)
+$(PKG)_DEPS      = cc ilmbase pthreads zlib $(BUILD)~cmake
+$(PKG)_PATCHES   = $(ilmbase_PATCHES)
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://www.openexr.com/downloads.html' | \
@@ -19,38 +19,19 @@ define $(PKG)_UPDATE
     head -1
 endef
 
-GCC_VERSION_MAJOR := $(shell echo $(gcc_VERSION) | cut -f1 -d.)
-GCC_VERSION_MINOR := $(shell echo $(gcc_VERSION) | cut -f2 -d.)
+GCC_VERSION_MAJOR := $(shell gcc --version | grep ^gcc | sed 's/^.* //g' | cut -f1 -d.)
+GCC_VERSION_MINOR := $(shell gcc --version | grep ^gcc | sed 's/^.* //g' | cut -f2 -d.)
 
-$(PKG)_CXXSTD_14 = $(shell if [[ $(GCC_VERSION_MAJOR) > 6 || \( $(GCC_VERSION_MAJOR) == 6 && $(GCC_VERSION_MINOR) >= 1 \) ]]; then echo true)
+$(PKG)_CXXSTD_14 = `[ $(GCC_VERSION_MAJOR) > 6 || \( $(GCC_VERSION_MAJOR) == 6 && $(GCC_VERSION_MINOR) >= 1 \) ] && echo true`
 
 define $(PKG)_BUILD
-    echo "patches: $(ilmbase_PATCHES)"
-    $(foreach PKG_PATCH,$(ilmbase_PATCHES), \
-    echo $(PKG_PATCH);\
-      (cd '$(SOURCE_DIR)' && $(PATCH) -p1 -u) < $(PKG_PATCH))
-    mkdir -p '$(BUILD_DIR)/native/IlmBase'
-    mkdir '$(BUILD_DIR)/cross'
-    cd '$(BUILD_DIR)/native/IlmBase' && cmake \
-    -DOPENEXR_CXX_STANDARD=$(if $($(PKG)_CXXSTD_14),14,11) \
-    -DCMAKE_INSTALL_PREFIX='$(BUILD_DIR)/native/IlmBase/install'\
-    '$(SOURCE_DIR)/IlmBase'
-    $(MAKE) -C '$(BUILD_DIR)/native/IlmBase' -j '$(JOBS)' install
-
-    cd '$(BUILD_DIR)/native/' && cmake \
-    -DOPENEXR_CXX_STANDARD=$(if $($(PKG)_CXXSTD_14),14,11) \
-    -DIlmBase_DIR='$(BUILD_DIR)/native/IlmBase/install/lib/cmake/IlmBase'\
-    '$(SOURCE_DIR)/OpenEXR'
-    $(MAKE) -C '$(BUILD_DIR)/native/IlmImf' -j '$(JOBS)'
- 
-     cd '$(BUILD_DIR)/cross' && $(TARGET)-cmake \
+     cd '$(BUILD_DIR)' && $(TARGET)-cmake \
     -DOPENEXR_CXX_STANDARD=$(if $($(PKG)_CXXSTD_14),14,11) \
     -DOPENEXR_INSTALL_PKG_CONFIG=ON \
-    -DNATIVE_OPENEXR_BUILD_DIR='$(BUILD_DIR)/native' \
     -DBUILD_TESTING=OFF \
     "$(SOURCE_DIR)/OpenEXR"
-    $(MAKE) -C '$(BUILD_DIR)/cross' -j '$(JOBS)'
-    $(MAKE) -C '$(BUILD_DIR)/cross' -j 1 install
+    $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
+    $(MAKE) -C '$(BUILD_DIR)' -j 1 install
 
     '$(TARGET)-g++' \
         -Wall -Wextra -std=c++$(if $($(PKG)_CXXSTD_14),14,11) \
